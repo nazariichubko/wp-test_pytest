f9f979519f745f8cdfc5294b3b9b33af2e542b42
nazar <chubkonazarii@gmail.com>
2022-06-17 22:23:28 +0300

    Added a new feature to add results of each test to sample .csv file.

--- tests/test_website_updated.py
+++ tests/test_website_updated.py
@@ -1,31 +1,30 @@
+import csv
+import datetime
 import pytest
-import requests
-from random import randrange
-import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from src.generators.player_localizations import PlayerLocalization
 from src.Classes.fake_data import FakeData as fd
-from selenium.webdriver.support import expected_conditions as EC
-
-from xlwt import Workbook
-
 
 driver = webdriver.Chrome(r"C:\Users\nazar\git\wp-test_pytest\src\webdriver_chrome_exe\chromedriver.exe")
-wb = Workbook()
-sheet = wb.add_sheet("test_result.csv")
-
-logging.basicConfig(filename='config_check.log', level=logging.INFO)
-logging.info('start')
-
+time_now  = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
 
 def test_open_calendar_page():
+    """Checkin if calendar page is reachable and opening."""
     driver.get('http://localhost/wp-test/calendar/test-calendar/')
     assert driver.current_url == r'http://localhost/wp-test/calendar/test-calendar/'
 
+    if driver.current_url == r'http://localhost/wp-test/calendar/test-calendar/':
+        with open('test_results.csv', 'a', newline='') as csv_file:
+            writer = csv.writer((csv_file))
+            writer.writerow([time_now, 'test_id_1', 'Calendar page is opened.', 'PASSED'])
+    else:
+        with open('test_results.csv', 'a', newline='') as csv_file:
+            writer = csv.writer((csv_file))
+            writer.writerow([time_now, 'Calendar page is NOT opened', 'Failed'])
+
 
 def test_current_date():
+    """Checking the current day value on calendar page"""
     driver.get('http://localhost/wp-test/calendar/test-calendar/')
     driver.implicitly_wait(5)
     today_date = driver.find_element(By.XPATH, "//td[contains(@class, 'simcal-today')]")
@@ -33,11 +32,17 @@ def test_current_date():
     assert today_date.is_displayed() == True
 
     if today_date.is_displayed() == True:
-        sheet.write(1, 0, f"Test Passed, Today's date: {today_date.text})")
-        wb.save("test_result.csv")
+        with open('test_results.csv', 'a', newline='') as csv_file:
+            writer = csv.writer((csv_file))
+            writer.writerow([time_now,'test_id_2', 'Today date displayed correctly', 'PASSED'])
+    else:
+        with open('test_results.csv', 'a', newline='') as csv_file:
+            writer = csv.writer((csv_file))
+            writer.writerow([time_now, 'Today data is not displayed', 'Failed'])
 
 
 def test_add_comment():
+    """Adding comment to the test website"""
     driver.get('http://localhost/wp-test/2022/05/10/test-post/')
     driver.implicitly_wait(5)
     comment_box = driver.find_element(By.ID, "comment")
@@ -54,9 +59,18 @@ def test_add_comment():
     comment_displayed = driver.find_element(By.XPATH, "//article[@class = 'comment clearfix']")
     assert comment_displayed.is_displayed() == True
 
+    if comment_displayed.is_displayed() == True:
+        with open('test_results.csv', 'a', newline='') as csv_file:
+            writer = csv.writer((csv_file))
+            writer.writerow([time_now,'test_id_3', 'Comment has been added', 'PASSED'])
+    else:
+        with open('test_results.csv', 'a', newline='') as csv_file:
+            writer = csv.writer((csv_file))
+            writer.writerow([time_now, 'Comment has NOT been added', 'Failed'])
+
     driver.quit()
 
 
-logging.info('done')
+
 
 
